fc399f6b96e33a6567f47eca523d08fe5202b628
taipioca <leanntai94@gmail.com>
2024-01-21 20:36:58 -0800

    tried

--- client/src/components/pages/Profile.tsx
+++ client/src/components/pages/Profile.tsx
@@ -16,23 +16,8 @@ const Profile = (props) => {
     <>
       <div className="Profile-avatarContainer">
         <div className="Profile-avatar" />
-      </div>
-      {/* <h1 className="Profile-name u-textCenter">{user.name}</h1> */}
-      <hr className="Profile-linejj" />
-      <div className="u-flex">
-        <div className="Profile-subContainer u-textCenter">
-          <h4 className="Profile-subTitle">About Me</h4>
-          <div id="profile-description">
-            I am really allergic to cats i don't know why i have a catbook
-          </div>
-        </div>
-        <div className="Profile-subContainer u-textCenter">
-          <h4 className="Profile-subTitle">Cat Happiness</h4>
-        </div>
-        <div className="Profile-subContainer u-textCenter">
-          <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
-          <div id="favorite-cat">corgi</div>
-        </div>
+      <h1 className="Profile-name u-textCenter">{props.userId}</h1>
+
       </div>
     </>
   );
