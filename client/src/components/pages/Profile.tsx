1056a9780652b9dc1efc024256df0ae98e8fdb5b
taipioca <leanntai94@gmail.com>
2024-01-23 14:00:40 -0800

    PROFILE CATALOG PEW PEW

--- client/src/components/pages/Profile.tsx
+++ client/src/components/pages/Profile.tsx
@@ -5,6 +5,7 @@ import "./Profile.css";
 import "../../utilities.css";
 import { NewReview } from "../modules/NewReview";
 import { NewItem } from "../modules/NewItem";
+import { Link } from "react-router-dom";
 
 interface User {
   name: string;
@@ -17,9 +18,21 @@ type Item = {
   image: string;
   title: string;
   points: number;
+  sharer: {
+    sharer_id: string;
+    sharer_name: string;
+  }
 };
 const Profile = (props) => {
   const [user, setUser] = useState<User>();
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    get("/api/catalog").then((itemsObjs: Item[]) => {
+      const userItems = itemsObjs.filter((item) => item.sharer.sharer_id === props.userId);
+      setItems(userItems);
+    });
+  }, [props.userId]);
 
   useEffect(() => {
     document.title = "Profile Page";
@@ -28,6 +41,7 @@ const Profile = (props) => {
   if (!user) {
     return <div> Loading! </div>;
   }
+
 
   return (
     <>
@@ -41,10 +55,26 @@ const Profile = (props) => {
         <h1 className="Profile-name u-textCenter">{user.name}</h1>
         <h2 className="Profile-points u-textCenter">{user.points} Points</h2>
         <h2 className="Profile-points u-textCenter">Rating: {user.rating}/5</h2>
-        <NewItem sharer_name={user.name} sharer_id = {props.userId}/>
+        <NewItem sharer_name={user.name} sharer_id={props.userId} />
 
-        <NewReview reviewerName={user.name} />
+        <NewReview reviewerName={user.name} reviewerId={props.userId} />
       </div>
+
+      <div className="catalog">
+      {items.map((item) => (
+        // <Link to={`/item/${item.id}`} key={item.id} id={item.id} className="item">
+        <div>
+          <div className="image-container">
+            <img src={item.image} alt={item.title} />
+          </div>
+          <h4 className="item-text">{item.title}</h4>
+          <p className="item-text">Rating: 5/5 (1 review)</p>
+          <h3 className="item-text">{item.points} Points/day</h3>
+          </div>
+
+        // </Link>
+      ))}
+    </div>
     </>
   );
 };
