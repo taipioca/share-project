6b4563eadc2cde1dda63dba50093ef648237e89f
Bran Tai <brandontai0313@gmail.com>
2024-01-22 17:08:15 -0800

    fix dabase crash

--- server/api.ts
+++ server/api.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import auth from "./auth";
 import socketManager from "./server-socket";
-const User = require("./models/user");
+// const User = require("./models/user");
 const router = express.Router();
 const Review = require("./models/review");
 
@@ -15,11 +15,11 @@ router.get("/whoami", (req, res) => {
   res.send(req.user);
 });
 
-router.get("/user", (req, res) => {
-  User.findById(req.query.userid).then((user) => {
-    res.send(user);
-  });
-});
+// router.get("/user", (req, res) => {
+//   User.findById(req.query.userid).then((user) => {
+//     res.send(user);
+//   });
+// });
 
 router.post("/initsocket", (req, res) => {
   // do nothing if user not logged in
@@ -41,13 +41,21 @@ router.post("/initsocket", (req, res) => {
 router.post("/newreview", auth.ensureLoggedIn, (req, res) => {
   const newReview = new Review({
     reviewer: {
-      reviewer_id: req.user?._id ?? "",
-      reviewer_name: req.user?.name ?? "",
+      reviewer_id: "123",
+      reviewer_name: "ABC",
     },
+    // reviewer: {
+    //   reviewer_id: req.user?._id ?? "",
+    //   reviewer_name: req.user?.name ?? "",
+    // },
     sharer: {
-      sharer_id: req.body.sharerId,
-      sharer_name: req.body.sharerName,
+      sharer_id: "456",
+      sharer_name: "DEF",
     },
+    // sharer: {
+    //   sharer_id: req.body.sharerId,
+    //   sharer_name: req.body.sharerName,
+    // },
     rating: "4.6",
     comment: "wauwwwww",
     timestamp: "2021-09-14T14:48:00.000Z",
